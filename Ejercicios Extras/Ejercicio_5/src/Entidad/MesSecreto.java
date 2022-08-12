ec0b231c1e75d587808564ba106dc879bc0c5973
Johana Carrizo <johanacarrizo20@gmail.com>
2022-08-11 21:16:47 -0300

    Cargando mas ejemplos

---
+++ Ejercicios Extras/Ejercicio_5/src/Entidad/MesSecreto.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidad;
+
+import java.util.Scanner;
+
+
+public class MesSecreto {
+        
+    Scanner leer = new Scanner(System.in);
+    String[] mes = {"enero","febrero","marzo","abril","mayo", "junio","julio","agosto","septiembre","octubre","noviembre","diciembre"};
+    String mesSecreto = mes[1];
+    
+    public void adivinarMes(){
+    
+        System.out.println("Adivine el mes: ");
+        
+        String a = leer.next().toLowerCase();
+        
+        while(!a.equals(mesSecreto)){
+        
+            System.out.println("No has acertado. Intentalo nuevamente: ");
+            a = leer.next().toLowerCase();
+        }
+        
+        System.out.println("Ha acertado!!!");
+    }
+}
